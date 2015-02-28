6334cf31366f2ee3fe69260357f1f769e863c19d
Marcos <mssjr200@gmail.com>
2015-02-28 12:53:49 -0300

    closes #5 criado tela para criação de usuario

--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from produtos.models import Produto
+from usuario.forms import UsuarioForm
 
 def painel(request):
 	produtos = Produto.objects \
@@ -13,3 +15,13 @@ def painel(request):
 		'produtos': produtos,
 		'total_produtos': qtd,	
 	})
+
+
+def criar_usuario(request):
+	form = UsuarioForm()
+	return render(request, 'usuario/criar.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
